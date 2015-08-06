60c5db05fc647d4f59b0c60b5e471756979f5af7
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-06 15:15:25 -0400

    Re-edit assignment
    Re-edit assignment

--- Cassini_Flavio_Output_Assignment/js/script.js
+++ Cassini_Flavio_Output_Assignment/js/script.js
@@ -7,3 +7,9 @@ var favoriteColorBlue = true; // My favorite color blue
 console.log(streetNumber);
 console.log(streetName);
 console.log(favoriteColorBlue);
+
+//Flavio Cassini
+//Student# 0004664025
+//08/06/2015
+//Instructor Lee Lewis
+//Program: Web Development & Design
