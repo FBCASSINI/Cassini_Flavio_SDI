e76e9727ab8c106028fb1cee025f89348914964d
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-06 14:52:53 -0400

    Assignment: Creating Variables and Output
    Assignment: Creating Variables and Output

---
+++ Assignment_Creating_Variables_and_Output/js/script.js
@@ -0,0 +1,9 @@
+//JavaScript
+
+var streetNumber = 19396; // My house street number
+var streetName = "liberty"; // My house street name
+var favoriteColorBlue = true; // My favorite color blue
+
+console.log(streetNumber);
+console.log(streetName);
+console.log(favoriteColorBlue);
