4061ef8bff8bcb60531f7f61b03aa76fc4a1c000
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-11 06:24:38 -0400

    added assigments
    added assignments

--- Cassini_Flavio_Expression_Assignment/js/script.js
+++ Cassini_Flavio_Expression_Assignment/js/script.js
@@ -2,12 +2,22 @@
 
 
 
-var tShirtPrice = prompt("Price of t-shirt");
-var tShirtQTY = prompt("How many do you want to buy");
-var pantsPrice = prompt("Price Of Pants")
-var pantsQTY = prompt ("How many do you want to buy")
-var shoppingCart = [Number(tShirtPrice), Number(tShirtQTY), Number(pantsPrice), Number(pantsQTY)]
-var totalPrice = shoppingCart[0] * shoppingCart[1] + shoppingCart[2] * shoppingCart[3]
-console.log(totalPrice)
+var tShirtPrice = prompt("Price of t-shirt?");
+var tShirtQTY = prompt("How many do you want to buy?");
+var pantsPrice = prompt("Price Of Pants?");
+var pantsQTY = prompt ("How many do you want to buy?");
+var shoppingCart = [Number(tShirtPrice), Number(tShirtQTY), Number(pantsPrice), Number(pantsQTY)];
+var totalPrice = shoppingCart[0] * shoppingCart[1] + shoppingCart[2] * shoppingCart[3];
+console.log(totalPrice);
 
 alert("Your total price is  $" + totalPrice);
+
+
+var shortsPrice = 20.00;
+shortsPrice *= 4
+
+console.log (shortsPrice);
+
+
+
+
