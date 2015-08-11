19fd8897a6d9380b0c443587984eff898d28365d
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-11 05:48:13 -0400

    assigned arrays to code
    assigned arrays to code

--- Cassini_Flavio_Expression_Assignment/js/script.js
+++ Cassini_Flavio_Expression_Assignment/js/script.js
@@ -4,7 +4,10 @@
 
 var tShirtPrice = prompt("Price of t-shirt");
 var tShirtQTY = prompt("How many do you want to buy");
-var totalPrice = tShirtPrice * tShirtQTY;
+var pantsPrice = prompt("Price Of Pants")
+var pantsQTY = prompt ("How many do you want to buy")
+var shoppingCart = [Number(tShirtPrice), Number(tShirtQTY), Number(pantsPrice), Number(pantsQTY)]
+var totalPrice = shoppingCart[0] * shoppingCart[1] + shoppingCart[2] * shoppingCart[3]
 console.log(totalPrice)
 
 alert("Your total price is  $" + totalPrice);
