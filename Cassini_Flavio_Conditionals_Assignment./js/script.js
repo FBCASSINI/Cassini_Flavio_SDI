f2fdbfd0e76bc16ef4e9d3261645750b580ca394
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-20 03:35:07 -0400

    added prompt backup
    added prompt backup

--- Cassini_Flavio_Conditionals_Assignment./js/script.js
+++ Cassini_Flavio_Conditionals_Assignment./js/script.js
@@ -6,14 +6,22 @@ Assignment: Conditionals*/
 
 
 var cookiePrice = prompt("Price of Cookie?"); //storing the input from user of the price of cookie
+(cookiePrice == "");
+    cookiePrice = prompt("Please enter the price of cookie.");
 console.log("Price of Cookie is $" + cookiePrice); //printing out variable cookie
 var cookieQTY = prompt("How many do you want to buy?"); //storing the input from the user of the quantity of cookie
+(cookieQTY == "");
+    cookieQTY = prompt("Please enter how many cookies you want to buy.");
 console.log(cookieQTY + " Cookies purchased"); //printing out variable cookieQTY
 var candyPrice = prompt("Price Of Candy?"); //Storing the input from the user of the price of Candy
+(candyPrice == "");
+    candyPrice = prompt("Please enter candy price.");
 console.log("Price of candy is $" + candyPrice); //printing out variable candyPrice
 var candyQTY = prompt ("How many do you want to buy?");//Storing the input from the user of the quantity of candy
+(candyQTY == "");
+   candyQTY = prompt("Please enter how much candy you want to buy.");
 console.log(candyQTY + " Candies purchased"); //Printing out Varaible pantsQTY
-var donation = confirm("Woukd you like to donate $10 to St. Jude Medical Research?");
+var donation = confirm("Would you like to donate $10 to St. Jude Medical Research?");
 console.log("Would you like to donate $10 to St. Jude Medical Research " + donation);
 var donation5 = confirm("would you also like to donate $5 to UNICEF");
 console.log("Would you like to donate to UNICEF?");
@@ -26,7 +34,7 @@ if(donation && donation5) {
 
     var totalPrice = shoppingCart[0] * shoppingCart[1] + shoppingCart[2] * shoppingCart[3] + shoppingCart[4] + shoppingCart[5]; //totaling all variables into array format
     console.log("Total price of food bought is $" + totalPrice); //printing out total price
-    alert("Your total price of food bought + $15 Donation to St. Jude Medical Research & UNICEF = $" + totalPrice); //printing out total to the user using alert box.
+    alert("Your total price of food bought + $15 Donation to St. Jude Medical Research & UNICEF total is $" + totalPrice); //printing out total to the user using alert box.
 
 }else if(donation > donation5) {
     var tenDonation2 = [10];
