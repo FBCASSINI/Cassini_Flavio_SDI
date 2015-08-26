a4fc7e5fd9eecf7a904a8cee5ea5838cb05ab40b
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-26 05:39:41 -0400

    added comments
    added comments

--- Cassini_Flavio_Functions_Assignment/js/script.js
+++ Cassini_Flavio_Functions_Assignment/js/script.js
@@ -3,18 +3,19 @@ Flavio Cassini
 Student Number: 0004664025
 SDI
 Assignment Functions
+Instructor Lee Lewis
  */
 
 //varaiables
-var tickets = prompt("How many Florida lottery tickets will you like to generate?");
+var tickets = prompt("How many Florida lottery tickets will you like to generate?"); //prompting for a number for how many to generate tickets
 
-var myRandom;
+var myRandom;// variablle myRandom to make the numbers raandom and round.
 
 
 
 //function to generate florida lottery numbers
 
-function numberValidate(number) {
+function numberValidate(number) {  //validating prompt to make sure a number is entered.
     while (number === "") {
         number = prompt("Please enter a number");
     }
@@ -22,12 +23,11 @@ function numberValidate(number) {
 }
 
 
-
-function randomNumGen(max, min, num){
+function randomNumGen(max, min, num){ //generating the numbers and also rounding.
     var randomNum = [];
     for (var i = 0; i < num; i++) {
         randomNum[i] = Math.random() * (max - min) + min;
-        randomNum[i] = Math.round(randomNum[i]);;
+        randomNum[i] = Math.round(randomNum[i]);
 
 
     }
@@ -35,16 +35,13 @@ function randomNumGen(max, min, num){
 
 }
 
-
-
 //function calls
-numberValidate(tickets);
-myRandom = randomNumGen(1,53,5);
-
+numberValidate(tickets); //function calling t validate the prompt
+myRandom = randomNumGen(1,53,5); //assigning variable of reandomNumGen to myRandom
 
 
 while (tickets > 0) {
-    console.log(myRandom = randomNumGen(1,53,5));
+    console.log(myRandom = randomNumGen(1,53,5)); //printing out the process as well as duplicating the amount of lotto tickets user asked for.
     tickets--
 
 }
