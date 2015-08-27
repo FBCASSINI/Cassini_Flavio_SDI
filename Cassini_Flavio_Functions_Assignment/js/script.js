b4b62e7f10fc8e0882871b879f6ea3adf965bcd1
Flavio Cassini <fbcassini@fullsail.edu>
2015-08-27 01:27:21 -0400

    final edit of the code
    added the 6 number in the lottery

--- Cassini_Flavio_Functions_Assignment/js/script.js
+++ Cassini_Flavio_Functions_Assignment/js/script.js
@@ -7,41 +7,43 @@ Instructor Lee Lewis
  */
 
 //varaiables
+
+var myRandom;// variablle myRandom to make the numbers random and round.
 var tickets = prompt("How many Florida lottery tickets will you like to generate?"); //prompting for a number for how many to generate tickets
-
-var myRandom;// variablle myRandom to make the numbers raandom and round.
-
 
 
 //function to generate florida lottery numbers
-
-function numberValidate(number) {  //validating prompt to make sure a number is entered.
-    while (number === "") {
-        number = prompt("Please enter a number");
-    }
-    alert("Thank You");
-}
-
-
-function randomNumGen(max, min, num){ //generating the numbers and also rounding.
-    var randomNum = [];
-    for (var i = 0; i < num; i++) {
-        randomNum[i] = Math.random() * (max - min) + min;
-        randomNum[i] = Math.round(randomNum[i]);
+    function numberValidate(number) {  //validating prompt to make sure a number is entered.
+        while (number === "") {
+            number = prompt("Please enter a number");
 
 
     }
-    return randomNum;
 
 }
+// I could not figure our how to join this while loop to tickets variable and call next funtion. Or make new function and have not interfere with the next.
+
+
+
+
+function randomNumGen(max, min, num) { //generating the numbers and also rounding.
+        var randomNum = [];
+        for (var i = 0; i < num; i++) {
+            randomNum[i] = Math.random() * (max - min) + min;
+            randomNum[i] = Math.round(randomNum[i]);
+        }
+        return randomNum;
+
+    }
 
 //function calls
 numberValidate(tickets); //function calling t validate the prompt
-myRandom = randomNumGen(1,53,5); //assigning variable of reandomNumGen to myRandom
+myRandom = randomNumGen(1,53,6); //assigning variable of reandomNumGen to myRandom
 
 
 while (tickets > 0) {
-    console.log(myRandom = randomNumGen(1,53,5)); //printing out the process as well as duplicating the amount of lotto tickets user asked for.
+    console.log( myRandom = randomNumGen(1,53,6) + " Florida lottery numbers"); //printing out the process as well as duplicating the amount of lotto tickets user asked for.
     tickets--
 
 }
+
